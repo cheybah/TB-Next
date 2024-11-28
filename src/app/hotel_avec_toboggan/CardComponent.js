c7eb7e45357226b4c6a707610091796e48d24525
cheybah <cheymabahroun@gmail.com>
2024-11-28 16:11:20 +0100

    fixed responsive design for Hotel Toboggan

--- src/app/hotel_avec_toboggan/CardComponent.js
+++ src/app/hotel_avec_toboggan/CardComponent.js
@@ -1,6 +1,3 @@
-import React from "react";
-
-
 const CardComponent = ({
     title,
     imageUrl,
@@ -12,21 +9,21 @@ const CardComponent = ({
 }) => {
     return (
         <div
-            className="flex p-3 bg-white rounded-md w-full mb-3"
+            className="flex flex-col lg:flex-row p-3 bg-white rounded-md w-full mb-3"
             style={{
                 fontFamily: "-apple-system, 'Roboto', Helvetica, Arial, sans-serif",
                 boxShadow: "0px 4px 16px 0px rgba(38, 38, 38, 0.17)",
             }}
         >
             {/* Left Section */}
-            <div className="relative h-full max-h-80 w-1/3 mr-5">
+            <div className="relative h-full max-h-80 lg:w-1/3 w-full lg:mr-5 mb-3 lg:mb-0">
                 {/* Badge */}
                 {showBadge && (
                     <div
-                        className="absolute bg-white/80 bg-opacity-90 shadow-sm rounded-md text-center px-3 py-2 text-sm font-medium text-gray-800"
+                        className="absolute bg-white/80 shadow-sm rounded-md text-center px-3 py-2 text-sm font-medium text-gray-800"
                         style={{
                             top: "5%",
-                            left: "25%",
+                            left: "32%",
                             transform: "translateX(-50%)",
                             zIndex: 10,
                             boxShadow: "0px 2px 3px 0px rgba(0, 0, 0, 0.24)",
@@ -50,42 +47,40 @@ const CardComponent = ({
             </div>
 
             {/* Middle Section */}
-            <div className="flex-grow">
+            <div className="flex-grow flex flex-col justify-between">
                 <div className="grid gap-1 mt-2">
-                    <span className="cursor-pointer font-bold text-2xl text-gray-800 underline w-full">
+                    <span className="cursor-pointer font-bold text-xl lg:text-2xl text-gray-800 underline w-full">
                         {title}
                     </span>
 
-                    <div className="flex items-center">
+                    <div className="flex items-center mt-2">
                         {Array.from({ length: 5 }, (_, index) => (
                             <svg
                                 key={index}
                                 xmlns="http://www.w3.org/2000/svg"
-                                width="24"  // Increase the size of the star (width and height)
+                                width="24"
                                 height="24"
-                                fill={index < rating ? "currentColor" : "none"} // Color the star if the rating is >= index
-                                stroke={index < rating ? "none" : "#4B5563"} // Dark gray for empty stars
+                                fill={index < rating ? "currentColor" : "none"}
+                                stroke={index < rating ? "none" : "#4B5563"}
                                 viewBox="0 0 24 24"
-                                className={`h-6 ${index < rating ? "text-gray-800" : "text-gray-400"}`} // h-6 increases the size (or you can control it with width/height)
+                                className={`h-6 ${
+                                    index < rating ? "text-gray-800" : "text-gray-400"
+                                }`}
                             >
-                                <path
-                                    d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"
-                                />
+                                <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
                             </svg>
                         ))}
                     </div>
                 </div>
             </div>
 
-
-
             {/* Divider */}
-            <div className="h-auto border-l mx-4"></div>
+            <div className="hidden lg:block h-auto border-l mx-4"></div>
 
             {/* Right Section */}
-            <div className="flex flex-col justify-between w-1/4">
+            <div className="flex flex-col justify-between w-full lg:w-1/4 mt-4 lg:mt-0">
                 {/* TripAdvisor Section */}
-                <div className="flex items-center mt-6">
+                <div className="flex items-center mb-4">
                     <img
                         className="w-10 h-10 mr-2"
                         src="https://tn.tunisiebooking.com/theme/images/icon_tripadvisor.svg"
@@ -99,10 +94,9 @@ const CardComponent = ({
 
                 {/* Button Section */}
                 <button
-                    className="bg-gradient-to-r from-red-500 to-pink-600 w-full py-2 text-white text-md font-bold rounded-md hover:opacity-90 flex items-center justify-center mb-24"
+                    className="bg-gradient-to-r from-red-500 to-pink-600 w-full py-2 text-white text-md font-bold rounded-md hover:opacity-90 flex items-center justify-center"
                     onClick={onButtonClick}
                 >
-                    {/* Calendar Icon */}
                     <img
                         className="w-5 h-5 mr-2"
                         src="https://tn.tunisiebooking.com/images/calendnew.svg"
--- src/app/hotel_avec_toboggan/page.js
+++ src/app/hotel_avec_toboggan/page.js
@@ -13,6 +13,9 @@ import {
   TabPanel,
 } from "@material-tailwind/react";
 import Footer from '../components/Footer/Footer';
+import './card.css';
+
+
 
 export default function HotelAvecTobogganPage() {
   const [expandedSections, setExpandedSections] = useState({
@@ -45,7 +48,7 @@ export default function HotelAvecTobogganPage() {
               tripAdvisorNote: "3.5/5",
               tripAdvisorLabel: "Bien",
               onButtonClick: () => console.log("Redirecting to Hammamet..."),
-              showBadge: true, 
+              showBadge: true,
             },
             {
               title: "Golden Tulip President Hammamet",
@@ -88,7 +91,7 @@ export default function HotelAvecTobogganPage() {
               tripAdvisorNote: "3.0/5",
               tripAdvisorLabel: "Moyen",
               onButtonClick: () => console.log("Redirecting to Sousse..."),
-              showBadge: true, 
+              showBadge: true,
             },
             {
               title: "Soviva Resort & Aquapark",
@@ -117,7 +120,7 @@ export default function HotelAvecTobogganPage() {
               tripAdvisorNote: "4.0/5",
               tripAdvisorLabel: "Très Bien",
               onButtonClick: () => console.log("Redirecting to Sousse..."),
-              showBadge: true, 
+              showBadge: true,
             },
             {
               title: "Abou Sofiane Hotel & Aquapark",
@@ -177,7 +180,7 @@ export default function HotelAvecTobogganPage() {
               tripAdvisorNote: "3.5/5",
               tripAdvisorLabel: "Bien",
               onButtonClick: () => console.log("Redirecting to Monastir..."),
-              showBadge: true, 
+              showBadge: true,
             },
             {
               title: "Amir Palace",
@@ -200,12 +203,12 @@ export default function HotelAvecTobogganPage() {
             },
             {
               title: "Blue Beach Golf & Spa",
-              imageUrl:"https://image.resabooking.com/images/image_panoramique/Blue_Beach_Golf_&_Spa_2.jpg",
+              imageUrl: "https://image.resabooking.com/images/image_panoramique/Blue_Beach_Golf_&_Spa_2.jpg",
               rating: "4",
               tripAdvisorNote: "4.5/5",
               tripAdvisorLabel: "Excellent",
               onButtonClick: () => console.log("Redirecting to Monastir..."),
-              showBadge: true, 
+              showBadge: true,
             },
             {
               title: "Iberostar Kuriat Palace",
@@ -237,7 +240,7 @@ export default function HotelAvecTobogganPage() {
               tripAdvisorNote: "3.5/5",
               tripAdvisorLabel: "Bien",
               onButtonClick: () => console.log("Redirecting to Mahdia..."),
-              showBadge: true, 
+              showBadge: true,
             },
             {
               title: "Mahdia Beach & Aquapark",
@@ -269,7 +272,7 @@ export default function HotelAvecTobogganPage() {
               tripAdvisorNote: "4.0/5",
               tripAdvisorLabel: "Très Bien",
               onButtonClick: () => console.log("Redirecting to Djerba..."),
-              showBadge: true, 
+              showBadge: true,
             },
             {
               title: "Djerba Castille",
@@ -298,7 +301,7 @@ export default function HotelAvecTobogganPage() {
               tripAdvisorNote: "4.0/5",
               tripAdvisorLabel: "Très Bien",
               onButtonClick: () => console.log("Redirecting to Djerba..."),
-              showBadge: true, 
+              showBadge: true,
             },
             {
               title: "Djerba Sun Beach",
@@ -339,7 +342,7 @@ export default function HotelAvecTobogganPage() {
   return (
     <div>
       <Header />
-      <div className="px-[15rem]">
+      <div className="sm:px-[15rem] md:px-[15rem] lg:px-[20rem]">
         <div className="w-full mx-auto">
           <Image
             src="/thematiq-min_(2).png"
@@ -349,24 +352,29 @@ export default function HotelAvecTobogganPage() {
             className="w-full h-auto" />
         </div>
         <Tabs id="custom-animation" value={activeTab} className="mt-6">
-          <TabsHeader
-            className="rounded-none border-b border-blue-gray-50 bg-transparent p-0"
-            indicatorProps={{
-              className: "bg-transparent border-b-2 border-gray-200 shadow-none rounded-none",
-            }}
-          >
-            {data.map(({ label, sublabel, value }) => (
-              <Tab
-                key={value}
-                value={value}
-                onClick={() => setActiveTab(value)}
-                className={`${activeTab === value ? "text-gray-900 bg-gray-200" : ""} px-4 py-2 rounded-t-md`}
-              >
-                {label}
-                <div className="text-sm text-gray-700">{sublabel}</div>
-              </Tab>
-            ))}
-          </TabsHeader>
+        <TabsHeader
+  className="rounded-none border-b border-blue-gray-50 bg-transparent p-0"
+  indicatorProps={{
+    className: "bg-transparent border-b-2 border-gray-200 shadow-none rounded-none",
+  }}
+  style={{
+    overflowX: "auto", // Enable horizontal scrolling
+    whiteSpace: "nowrap", // Prevent wrapping of tab items
+  }}
+>
+  {data.map(({ label, sublabel, value }) => (
+    <Tab
+      key={value}
+      value={value}
+      onClick={() => setActiveTab(value)}
+      className={`${activeTab === value ? "text-gray-900 bg-gray-200" : ""} px-4 py-2 rounded-t-md inline-block`}
+    >
+      {label}
+      <div className="text-sm text-gray-700">{sublabel}</div>
+    </Tab>
+  ))}
+</TabsHeader>
+
           <TabsBody
             animate={{
               initial: { y: 250 },
@@ -386,88 +394,114 @@ export default function HotelAvecTobogganPage() {
           </TabsBody>
         </Tabs>
 
-        <div className="text-left mt-4">
-
-          <div className="text-2xl font-bold">Hotel Avec Toboggan : Tout le monde y trouve son bonheur 100% glisse!
+        <div className="text-left mt-4 px-4 sm:px-6 md:px-8">
+          {/* Title */}
+          <div className="text-2xl sm:text-3xl font-bold">
+            Hotel Avec Toboggan : Tout le monde y trouve son bonheur 100% glisse!
           </div>
+
+          {/* Divider */}
           <div className="w-[5rem] h-[4px] bg-black mt-2"></div>
+
+          {/* Collapsible Section */}
           <div className={`overflow-hidden transition-all duration-300 ${expandedSections.section1 ? 'max-h-none' : 'max-h-[6rem]'}`}>
-            <p className='mt-4'>
-              Vous etes a la recherche d une destination pour vos vacances ? Pourquoi ne pas choisir de visiter le beau pays qu'est la Tunisie. Pourquoi faire ce choix? Parmi les nombreux arguments que l'on pourrait vous soumettre, le climat est sans aucun doute celui qui peut faire toute la difference.
+            <p className="mt-4 text-sm sm:text-base md:text-lg">
+              Vous êtes à la recherche d’une destination pour vos vacances ? Pourquoi ne pas choisir de visiter le beau pays qu'est la Tunisie. Pourquoi faire ce choix? Parmi les nombreux arguments que l'on pourrait vous soumettre, le climat est sans aucun doute celui qui peut faire toute la différence.
             </p>
             <div className="mt-4">
-              <p>
-                Le sait tous que le climat du pays est chaud et sec lors des vacances d'ete, en hiver, il y fait plus frais et doux,<br />
-                en particulier dans le sud. La temperature varie aux alentours de 13 C durant le mois d'hiver. Ce qui ne vous empechera surement pas de profiter des diverses attractions aquatiques que plusieurs hotels du pays proposent. Les hotel avec toboggan sont nombreux en Tunisie. Ils vous offrent diverses activites pour rendre votre séjour inoubliables.
+              <p className="text-sm sm:text-base md:text-lg">
+                Le sait tous que le climat du pays est chaud et sec lors des vacances d'été, en hiver, il y fait plus frais et doux, <br />
+                en particulier dans le sud. La température varie aux alentours de 13°C durant le mois d'hiver. Ce qui ne vous empêchera sûrement pas de profiter des diverses attractions aquatiques que plusieurs hôtels du pays proposent. Les hôtels avec toboggan sont nombreux en Tunisie. Ils vous offrent diverses activités pour rendre votre séjour inoubliable.
               </p>
             </div>
           </div>
-          <button onClick={() => toggleContent('section1')} className="mt-0 text-blue-500 flex items-center space-x-2">
+
+          {/* Toggle Button */}
+          <button
+            onClick={() => toggleContent('section1')}
+            className="mt-2 text-blue-500 flex items-center space-x-2"
+          >
             <span>{expandedSections.section1 ? "Afficher moins" : "Afficher plus"}</span>
-            <FontAwesomeIcon icon={faChevronDown} className={`transition-transform duration-300 ${expandedSections.section1 ? 'rotate-180' : 'rotate-0'}`} />
+            <FontAwesomeIcon
+              icon={faChevronDown}
+              className={`transition-transform duration-300 ${expandedSections.section1 ? 'rotate-180' : 'rotate-0'}`}
+            />
           </button>
         </div>
 
-        <div className="text-left mt-4">
-          <div className="text-2xl font-bold">Découvrez les toboggans d'hotel les plus fous en Tunisie :
+        <div className="text-left mt-4 px-4 sm:px-6 md:px-8">
+          {/* Title */}
+          <div className="text-2xl sm:text-3xl font-bold">
+            Découvrez les toboggans d'hôtel les plus fous en Tunisie :
           </div>
           <div className="w-[5rem] h-[4px] bg-black mt-2"></div>
+
+          {/* Collapsible Section 1 */}
           <div className={`overflow-hidden transition-all duration-300 ${expandedSections.section2 ? 'max-h-none' : 'max-h-[6rem]'}`}>
-            <p className='mt-4'>
-              Vous avez fait le choix de vivre une experience unique dans ce splendide pays? Voici une liste des meilleurs hotel avec toboggan aquatique tunisie ou vous ne risquez pas de vous ennuyer, que vous avez choisi de faire un petit sejour en solitaire, avec un groupe d'amis ou encore mieux, avec toute la famille.
+            <p className="mt-4 text-sm sm:text-base md:text-lg">
+              Vous avez fait le choix de vivre une expérience unique dans ce splendide pays? Voici une liste des meilleurs hôtels avec toboggan aquatique en Tunisie où vous ne risquez pas de vous ennuyer, que vous ayez choisi de faire un petit séjour en solitaire, avec un groupe d'amis ou encore mieux, avec toute la famille.
             </p>
-            <h3 className="text-left text-lg font-semibold mt-4">Toboggan aquatique a Hammamet :</h3>
-            <p>
-              Se situant au nord-est du pays, Hammamet fait partie des principales stations balneaires de la Tunisie.
-              On y trouve les meilleures attractions aquatiques du pays, parmi les hotel avec toboggan aquatique a hammamet, on peut citer l'hotel 4 etoiles Hammamet.Le Zodiac avec ses deux piscines interieure et exterieure, la piscine interieure est chauffee. Le Zodiac propose une des meilleures attractions aquatiques de la ville de Hammamet avec son somptueux toboggan. Mais parmi les hotels avec toboggan hammamet yasmine, on a aussi l'hotel Yasmine Beach qui propose un excellent service.
+
+            {/* Toboggan Sections */}
+            <h3 className="text-left text-lg sm:text-xl font-semibold mt-4">Toboggan aquatique à Hammamet :</h3>
+            <p className="text-sm sm:text-base md:text-lg">
+              Se situant au nord-est du pays, Hammamet fait partie des principales stations balnéaires de la Tunisie. On y trouve les meilleures attractions aquatiques du pays, parmi les hôtels avec toboggan aquatique à Hammamet, on peut citer l'hôtel 4 étoiles Hammamet Le Zodiac avec ses deux piscines intérieure et extérieure. La piscine intérieure est chauffée. Le Zodiac propose une des meilleures attractions aquatiques de la ville de Hammamet avec son somptueux toboggan.
             </p>
-            <h3 className="text-left text-lg font-semibold mt-4">Hotel avec Toboggan à Djerba :</h3>
-            <p>
-              Magnifique ville situee dans la mediterranee, Djerba fait en tout 514 km2. La legende raconte qu'Ulysse aurait traverse la ville lors de son voyage de retour. L'hotel Club Caribbean est un hotel avec toboggan djerba se trouvant sur la cote ouest de la ville de Djerba.
-              En plus de sa piscine avec toboggan, il propose aussi diverses attractions aquatiques pour enfants.
+
+            <h3 className="text-left text-lg sm:text-xl font-semibold mt-4">Hôtel avec Toboggan à Djerba :</h3>
+            <p className="text-sm sm:text-base md:text-lg">
+              Magnifique ville située dans la Méditerranée, Djerba fait en tout 514 km². La légende raconte qu'Ulysse aurait traversé la ville lors de son voyage de retour. L'hôtel Club Caribbean est un hôtel avec toboggan à Djerba se trouvant sur la côte ouest de la ville de Djerba. En plus de sa piscine avec toboggan, il propose aussi diverses attractions aquatiques pour enfants.
             </p>
-            <h3 className="text-left text-lg font-semibold mt-4">Hotel avec Toboggan à Sousse : </h3>
-            <p>
-              Superbe ville portuaire situee a l'Est du pays, Sousse, aussi connue du grand public sous le surnom perle du Sahel est la 3eme plus grande ville de Tunisie. Elle propose divers sites historiques, dont sa medina qui fait partie du patrimoine mondial de l'UNESCO a partir de 1988. Parmi les nombreux hotel avec toboggan sousse Tunisie, on a Tej Marhaba qui est un complexe touristique situe a seulement 1km du centre-ville.
-              Il propose plusieurs activites, dont deux piscines pour adultes et une pour enfant.
+
+            <h3 className="text-left text-lg sm:text-xl font-semibold mt-4">Hôtel avec Toboggan à Sousse :</h3>
+            <p className="text-sm sm:text-base md:text-lg">
+              Superbe ville portuaire située à l'Est du pays, Sousse, aussi connue sous le surnom "perle du Sahel", est la 3ème plus grande ville de Tunisie. Elle propose divers sites historiques, dont sa médina qui fait partie du patrimoine mondial de l'UNESCO depuis 1988. Parmi les nombreux hôtels avec toboggan à Sousse, on a Tej Marhaba, un complexe touristique situé à seulement 1 km du centre-ville. Il propose plusieurs activités, dont deux piscines pour adultes et une pour enfants.
             </p>
-            <h3 className="text-left text-lg font-semibold mt-4">Hotel avec Toboggan à Mahdia : </h3>
-            <p>
-              Situee a plus de 200 km dans l Est de la Tunisie, Mahdia est une belle ville cotiere du pays. Elle est surtout connue pour son port de peche qui a ete le tout premier jamais construit dans la ville. L'hotel avec toboggan mahdia Beach est un des meilleurs hotel de la ville de Mahdia. Enseigne 4 etoiles, Mahdia Beach propose 3 splendides piscines, dont une magnifique piscine exterieure disposant de toboggan.
-              Mais on a aussi une piscine pour enfant et une exterieure.
+
+            <h3 className="text-left text-lg sm:text-xl font-semibold mt-4">Hôtel avec Toboggan à Mahdia :</h3>
+            <p className="text-sm sm:text-base md:text-lg">
+              Située à plus de 200 km à l'est de la Tunisie, Mahdia est une belle ville côtière du pays. Elle est surtout connue pour son port de pêche, qui a été le tout premier jamais construit dans la ville. L'hôtel Mahdia Beach est un des meilleurs hôtels de la ville. Enseigne 4 étoiles, Mahdia Beach propose 3 splendides piscines, dont une magnifique piscine extérieure disposant de toboggan.
             </p>
-            <h3 className="text-left text-lg font-semibold mt-4">Hotel avec Toboggan à Monastir : </h3>
-            <p>
-              Monastir se trouve a quelques 20 km a l est de la ville de Sousse, belle ville cotiere du Sahel, Monastir est installe sur une presqu ile du sud-est du golf d'Hammamet.
-              Le Sahara Beach fait partie des hotels avec toboggan Monastir les plus connus de la ville.
+
+            <h3 className="text-left text-lg sm:text-xl font-semibold mt-4">Hôtel avec Toboggan à Monastir :</h3>
+            <p className="text-sm sm:text-base md:text-lg">
+              Monastir se trouve à quelques 20 km à l'est de la ville de Sousse. Belle ville côtière du Sahel, Monastir est installée sur une presqu'île du sud-est du golfe d'Hammamet. Le Sahara Beach fait partie des hôtels avec toboggan les plus connus de la ville.
             </p>
           </div>
+
+          {/* Toggle Button */}
           <div>
-            <button onClick={() => toggleContent('section2')} className="mt-0 text-blue-500 flex items-center space-x-2">
+            <button onClick={() => toggleContent('section2')} className="mt-2 text-blue-500 flex items-center space-x-2">
               <span>{expandedSections.section2 ? "Afficher moins" : "Afficher plus"}</span>
               <FontAwesomeIcon icon={faChevronDown} className={`transition-transform duration-300 ${expandedSections.section2 ? 'rotate-180' : 'rotate-0'}`} />
             </button>
           </div>
 
+          {/* Collapsible Section 2 */}
           <div className="text-left mt-4">
-            <div className="text-2xl font-bold">Pensez aux vacances en hotel avec Toboggan Aquatiques !
+            <div className="text-2xl sm:text-3xl font-bold">
+              Pensez aux vacances en hôtel avec Toboggan Aquatiques !
             </div>
             <div className="w-[5rem] h-[4px] bg-black mt-2"></div>
-            <div className={`overflow-hidden transition-all duration-300 ${expandedSections.section3 ? 'max-h-none' : 'max-h-[6rem]'}`}>                    <p className='mt-4'>
-              Parmi les divers avantages de choisir un hotel avec toboggan est sans aucun doute la garantie d'un sejour dans la bonne humeur et loin de l ennui. Mais avant tout, il faut bien choisir son pays de destination en fonction de son climat.                         </p>
+            <div className={`overflow-hidden transition-all duration-300 ${expandedSections.section3 ? 'max-h-none' : 'max-h-[6rem]'}`}>
+              <p className="mt-4 text-sm sm:text-base md:text-lg">
+                Parmi les divers avantages de choisir un hôtel avec toboggan est sans aucun doute la garantie d'un séjour dans la bonne humeur et loin de l'ennui. Mais avant tout, il faut bien choisir son pays de destination en fonction de son climat.
+              </p>
               <div className="mt-4">
-                <p>
-                  Tunisie dispose de tous ces atouts, pour vos prochaines vacances, n'oubliez pas de reserver un hotel avec toboggan Tunisie.
+                <p className="text-sm sm:text-base md:text-lg">
+                  La Tunisie dispose de tous ces atouts, pour vos prochaines vacances, n'oubliez pas de réserver un hôtel avec toboggan en Tunisie.
                 </p>
               </div>
             </div>
           </div>
 
-          <button onClick={() => toggleContent('section3')} className="mt-0 text-blue-500 flex items-center space-x-2">
+          {/* Toggle Button for Section 2 */}
+          <button onClick={() => toggleContent('section3')} className="mt-2 text-blue-500 flex items-center space-x-2">
             <span>{expandedSections.section3 ? "Afficher moins" : "Afficher plus"}</span>
             <FontAwesomeIcon icon={faChevronDown} className={`transition-transform duration-300 ${expandedSections.section3 ? 'rotate-180' : 'rotate-0'}`} />
           </button>
         </div>
+
 
       </div>
       <Footer />
